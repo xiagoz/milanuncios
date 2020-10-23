d6c75113b9bc5acda7e709c61311ff7384fd7004
Christian Gómez 2 <61128589+tiangoz@users.noreply.github.com>
2020-10-23 00:59:29 -0500

    base de datos

---
+++ bd_milanuncios.sql
@@ -0,0 +1,137 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 23-10-2020 a las 07:51:01
+-- Versión del servidor: 10.4.11-MariaDB
+-- Versión de PHP: 7.4.3
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `bd_milanuncios`
+--
+
+DELIMITER $$
+--
+-- Procedimientos
+--
+CREATE DEFINER=`root`@```localhost``` PROCEDURE `listar_categorias` ()  BEGIN
+select * from categorias;
+END$$
+
+CREATE DEFINER=`root`@`localhost` PROCEDURE `listar_filtros` (IN `param_campo` VARCHAR(255), IN `param_valor` VARCHAR(255))  BEGIN
+SET @S = CONCAT("SELECT id, id_categorias, nombre_subcategoria, titulo_anuncio, descripcion from sub_categorias where ", param_campo ," LIKE CONCAT('%", param_valor ,"%')");
+
+PREPARE stmt FROM @S;
+EXECUTE stmt;
+DEALLOCATE PREPARE stmt;
+
+END$$
+
+CREATE DEFINER=`root`@```localhost``` PROCEDURE `listar_subcategorias` ()  BEGIN
+select * from sub_categorias;
+END$$
+
+DELIMITER ;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `categorias`
+--
+
+CREATE TABLE `categorias` (
+  `id` int(11) NOT NULL,
+  `nombre_categoria` varchar(25) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `categorias`
+--
+
+INSERT INTO `categorias` (`id`, `nombre_categoria`) VALUES
+(1, 'Promociones'),
+(2, 'Ofertas');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `sub_categorias`
+--
+
+CREATE TABLE `sub_categorias` (
+  `id` int(11) NOT NULL,
+  `id_categorias` int(11) NOT NULL,
+  `nombre_subcategoria` varchar(25) NOT NULL,
+  `titulo_anuncio` varchar(100) NOT NULL,
+  `descripcion` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `sub_categorias`
+--
+
+INSERT INTO `sub_categorias` (`id`, `id_categorias`, `nombre_subcategoria`, `titulo_anuncio`, `descripcion`) VALUES
+(1, 1, 'Restaurantes', 'Rapid Pizza', 'Pizza Familiar con 10% de descuento'),
+(2, 1, 'Viajes', 'Gorgona', 'Espectacular paseo a gorgona'),
+(3, 2, 'Bares', 'Cerveza y Alitas', 'disfruta con un descuento del 10%'),
+(4, 2, 'Mecanica Automotriz', 'Manteniemiento Preventivo', 'mantieniemto del aire incluido');
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `categorias`
+--
+ALTER TABLE `categorias`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `sub_categorias`
+--
+ALTER TABLE `sub_categorias`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_categorias` (`id_categorias`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `categorias`
+--
+ALTER TABLE `categorias`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT de la tabla `sub_categorias`
+--
+ALTER TABLE `sub_categorias`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `sub_categorias`
+--
+ALTER TABLE `sub_categorias`
+  ADD CONSTRAINT `sub_categorias_ibfk_1` FOREIGN KEY (`id_categorias`) REFERENCES `categorias` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
